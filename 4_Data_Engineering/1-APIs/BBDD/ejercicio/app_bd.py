b9df9753397b4af5c231ef0a35c8b3060984514d
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2025-02-26 12:43:17 +0100

    W12 D3 sol api local

--- 4_Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
+++ 4_Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, requests, HTTPException
 from pydantic import BaseModel
+from typing import List
 import sqlite3
 import uvicorn
 
 app = FastAPI()
 
 class Book(BaseModel):
+    # id: None
     published: int
     author: str
     title: str
@@ -15,15 +17,55 @@ class Book(BaseModel):
 conn = sqlite3.connect('books.db')
 cursor = conn.cursor()
 
+def query_db(results, cursor):
+    
+    # Obtener los nombres de las columnas
+    column_names = [column[0] for column in cursor.description]
+    
+    # Convertir cada tupla en un diccionario
+    books = [dict(zip(column_names, book)) for book in results]
+    return books
+
 @app.get("/")
 async def hello():
     return "Hello world"
 
 # 1.Ruta para obtener todos los libros
+@app.get("/v1/books", response_model=List[Book])
+async def get_books():
+    query = "SELECT published,author,title,first_sentence FROM books"
+    results = cursor.execute(query).fetchall()
+    return query_db(results, cursor)
 
 
 # 2.Ruta para obtener los libros de un autor
-
+@app.get("/v1/author/{author}/books", response_model=List[Book])
+async def get_author_books(author:str):
+    query = """
+    SELECT published,
+           author,
+           title,
+           first_sentence 
+    FROM books
+    WHERE author LIKE ?
+    """
+    results =  cursor.execute(query, (f"%{author}%",)).fetchall()
+    return query_db(results, cursor)
 
 # 3.Ruta para añadir un libro
+@app.post("/v1/books")
+async def create_book(book: Book):
+    try:
+        query = """
+        INSERT INTO books (published, author, title, first_sentence)
+        VALUES (?,?,?,?)
+        """
+        cursor.execute(query, (book.published, book.author, book.title, book.first_sentence))
+        conn.commit()
+        return {"message": f"Book {book.title} added sucessfully."}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error inserting the book")
 
+
+
+uvicorn.run(app)
---
+++ 4_Data_Engineering/2-Docker/Practica/app_model.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, HTTPException
+import pickle
+import uvicorn
+# from pydantic import BaseModel
+import sqlite3
+import pandas as pd
+
+app = FastAPI()
+
+
+# class InputData(BaseModel):
+#     tv: int
+#     radio: int
+#     newspaper: int
+
+
+@app.get("/")
+async def hello():
+    return {"message": "Bienvenido a mi API del modelo de Advertising"}
+
+# 1. Endpoint de predicción
+@app.post("/predict")
+async def predict(input:dict):
+    model = pickle.load(open("data/advertising_model.pkl", 'rb'))
+    data = input.get("data")
+    if not data:
+        raise HTTPException(status_code=400, detail="No se han proporcionado datos")
+    try:
+        prediction = model.predict(data)
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al realizar la predicción. ")
+
+
+# 2. Endpoint de ingesta de datos
+@app.post("/ingest")
+async def ingest(input:dict):
+    data = input.get("data")
+    if not data:
+        raise HTTPException(status_code=400, detail="No se han proporcionado datos")
+    try:
+        conn = sqlite3.connect('./data/advertising.db')
+        cursor = conn.cursor()
+        query = """INSERT INTO campañas VALUES (?,?,?,?)"""
+        for fila in data:
+            cursor.execute(query, fila)
+        conn.commit()
+        conn.close()
+        return {"message": 'Datos ingresados correctamente'}
+    except Exception as e:
+            raise HTTPException(status_code=500, detail="Error al ingestar los datos. ")
+
+# 3. Endpoint de reentramiento del modelo
+@app.post("/retrain")
+async def retrain():
+    try:
+        conn = sqlite3.connect('./data/advertising.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM campañas"
+        result = cursor.execute(query)
+        df = pd.DataFrame(result)
+        conn.close()
+        model = pickle.load(open("data/advertising_model.pkl", 'rb'))
+        model.fit(df.iloc[:,:-1], df.iloc[:,-1])
+        pickle.dump(model, open('./data/advertising_model.pkl', 'wb'))
+        return {'message': 'Modelo reentrenado correctamente.'}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al reentrenar el modelo. ")
+
+
+uvicorn.run(app)
